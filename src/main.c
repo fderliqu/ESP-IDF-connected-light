b6f18dce58c2f8da68c749621ec283af168ea95e
Florian Derlique <florian.derlique.etu@univ-lille.fr>
2022-06-04 16:16:24 +0200

    Add some SPIFFS functions

--- src/main.c
+++ src/main.c
@@ -9,19 +9,22 @@
 //----------------------------INCLUDES
 
 #include <string.h>
+#include <dirent.h>
+#include <sys/unistd.h>
+#include <sys/stat.h>
 #include"esp_wifi.h"
 #include"esp_log.h"
 #include"esp_event.h"
+#include "esp_spiffs.h"
 #include"freertos/FreeRTOS.h"
 #include"freertos/task.h"
 #include"freertos/event_groups.h"
 #include"nvs_flash.h"
 
-
-
 //----------------------------DEFINES/GLOBALS
-#define ESP_WIFI_SSID      "Redmi Note 10S"
-#define ESP_WIFI_PASS      "fd3909op"
+#define ESP_WIFI_SSID       "Redmi Note 10S"
+#define ESP_WIFI_PASS       "fd3909op"
+#define ESP_SPIFFS_PATH     "/spiffs"    
 #define ESP_WIFI_MAXIMUM_RETRY  10
 #define MAX_AP 16
 
@@ -44,6 +47,8 @@ bool status_scan = false, status_connected = false;
 
 //TAGS for ESP LOGGING
 static const char *TAG_wifi_sta = "[ESP-Wifi]";
+static const char *TAG_spiffs = "[ESP-SPIFFS]";
+static const char *TAG_server = "[ESP-SERVER]";
 
 //Wifi_station retry counter
 static int wifi_sta_retry_num = 0;
@@ -300,6 +305,83 @@ static void connect_wifi(){
 //----------------------------SERVER_FUNCTION
 
 //----------------------------SPIFFS_FUNCTION
+
+esp_err_t init_spiffs(){
+    ESP_LOGI(TAG_spiffs,"Initialize SPIFFS");
+
+    //Create config
+    esp_vfs_spiffs_conf_t spiffs_config = {
+        .base_path = ESP_SPIFFS_PATH,
+        .partition_label = NULL,
+        .max_files = 10,
+        .format_if_mount_failed = true
+    };
+    esp_err_t status = esp_vfs_spiffs_register(&spiffs_config);
+    if(status == ESP_FAIL)ESP_LOGE(TAG_spiffs,"Failed to mount or format filesystem");
+    else if(status == ESP_ERR_NOT_FOUND)ESP_LOGE(TAG_spiffs,"Failed to find SPIFFS partition");
+    else if(status == ESP_ERR_NO_MEM)ESP_LOGE(TAG_spiffs,"Failed to allocate objects");
+    else if(status == ESP_ERR_INVALID_STATE)ESP_LOGE(TAG_spiffs,"Already mounted or partition is encrypted");
+    if(!ESP_OK)return status;
+
+    //Get infos
+    size_t total = 0, used = 0;
+    status = esp_spiffs_info(spiffs_config.partition_label,&total,&used);
+    if(!ESP_OK){
+        ESP_LOGE(TAG_spiffs,"Failed to get SPIFFS partition information (%s). Formatting...",esp_err_to_name(status));
+        return status;
+    }
+    else{
+        ESP_LOGI(TAG_spiffs, "Partition size: total: %d, used: %d", total, used);
+    }
+
+    //Check is used > total
+    if(used>total){
+        ESP_LOGE(TAG_spiffs,"Number of used bytes cannot be larger than total");
+        return ESP_FAIL;
+    }
+    return status;
+}
+
+esp_err_t print_spiffs_files(){
+    char * dirpath = ESP_SPIFFS_PATH;
+    DIR *dir = opendir(dirpath);
+    size_t dirpath_len = strlen(dirpath);
+    if(!dir){
+        ESP_LOGE(TAG_spiffs,"Failed to open directory : %s",dirpath);
+        return ESP_FAIL;
+    }
+    ESP_LOGI(TAG_spiffs,"Checking spiffs files");
+    struct dirent *entry;
+    while((entry = readdir(dir)) != NULL){
+        ESP_LOGI(TAG_spiffs,"Found : name : %s | type %s",entry->d_name,entry->d_type);
+    }
+    return ESP_OK;
+}
+
+esp_err_t exist_file_spiffs(const char * file){
+    char * dirpath = ESP_SPIFFS_PATH;
+    bool check_status = false;
+    DIR *dir = opendir(dirpath);
+    size_t dirpath_len = strlen(dirpath);
+    if(!dir){
+        ESP_LOGE(TAG_spiffs,"Failed to open directory : %s",dirpath);
+        return ESP_FAIL;
+    }
+    struct dirent *entry;
+    while(!check_status){
+        if((entry = readdir(dir)) != NULL){
+            if(strcmp(entry->d_name,file) == 0){
+                closedir(dir);
+                return ESP_OK;
+            }
+        }
+        else{
+            closedir(dir);
+            return ESP_FAIL;
+        }
+    }
+}
+
 
 //----------------------------MAIN
 
